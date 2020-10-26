8c6229ebaaa19f1306d6842f2059cde42619420c
Yuri Burk <yuriburk@gmail.com>
2020-10-25 21:10:13 -0300

    First style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,5 @@
-import Head from 'next/head';
+import Main from 'components/Main';
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1>Next.js</h1>
-      </main>
-    </div>
-  );
+  return <Main />;
 }
